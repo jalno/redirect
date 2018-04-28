4b3f71f7206e907cfa456419a3f584040838ef1e
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2018-04-28 14:37:02 +0430

    check regex addresses

--- controllers/redirect.php
+++ controllers/redirect.php
@@ -14,7 +14,8 @@ class redirect extends controller{
 		$address = new address();
 		$address->where('status', address::active);
 		foreach($address->get() as $address){
-			if($address->isRegex() and $destination = preg_replace($address->source, $address->destination, $uri)){
+			if($address->isRegex() and preg_match($address->source, $uri)){
+				$destination = preg_replace($address->source, $address->destination, $uri);
 				$address->hit();
 				$this->response->setHttpCode($address->type);
 				$this->response->Go($destination);
