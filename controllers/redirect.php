f12aac4e884aefd8678c7c9d27d6957e38b9f390
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2020-12-26 02:52:51 +0330

    fix php error about response property

--- controllers/redirect.php
+++ controllers/redirect.php
@@ -1,25 +1,21 @@
 <?php
 namespace packages\redirect\controllers;
-use \packages\base\http;
-use \packages\base\response;
-use \packages\base\controller;
-use \packages\redirect\address;
-class redirect extends controller{
-	private $response;
-	public function __construct(){
-		$this->response = new response();
-	}
+
+use packages\base\{http, Controller};
+use packages\redirect\Address;
+
+class Redirect extends Controller {
 	public function redirector(){
 		$uri = http::getURL();
-		$address = new address();
-		$address->where('status', address::active);
-		foreach($address->get() as $address){
-			if($address->isRegex() and preg_match($address->source, $uri)){
+		$address = new Address();
+		$address->where("status", Address::active);
+		foreach($address->get() as $address) {
+			if ($address->isRegex() and preg_match($address->source, $uri)) {
 				$destination = preg_replace($address->source, $address->destination, $uri);
 				$address->hit();
 				$this->response->setHttpCode($address->type);
 				$this->response->Go($destination);
-			}elseif($address->source == $uri){
+			} elseif($address->source == $uri) {
 				$address->hit();
 				$this->response->setHttpCode($address->type);
 				$this->response->Go($address->destination);
